29b6f5843dbeeca1e7c1d49ff6e3af951787ccda
James Bonner <james@aiva.cloud>
2023-03-20 19:17:05 +1100

    set up RenderableEntity + subclasses and use them to render (triangle for fleet is broken :( )

--- planet_wars_draw.py
+++ planet_wars_draw.py
@@ -1,13 +1,17 @@
-from entities import NEUTRAL_ID
 import pyglet
+from entities import NEUTRAL_ID
 
+PLANET_RADIUS_FACTOR = 20
+
+#region colours
 COLOR_NAMES = {
 	"BLACK": (0.0, 0.0, 0.0, 1),
 	"WHITE": (1.0, 1.0, 1.0, 1),
+	"GREY": (0.6, 0.6, 0.6, 1),
+	"LIGHT_GREY": (0.9, 0.9, 0.9, 1),
 	"RED": (1.0, 0.0, 0.0, 1),
 	"GREEN": (0.0, 1.0, 0.0, 1),
 	"BLUE": (0.0, 0.0, 1.0, 1),
-	"GREY": (0.6, 0.6, 0.6, 1),
 	"PINK": (1.0, 0.7, 0.7, 1),
 	"YELLOW": (1.0, 1.0, 0.0, 1),
 	"ORANGE": (1.0, 0.7, 0.0, 1),
@@ -16,29 +20,86 @@ COLOR_NAMES = {
 	"AQUA": (0.0, 1.0, 1.0, 1),
 	"DARK_GREEN": (0.0, 0.4, 0.0, 1),
 	"LIGHT_BLUE": (0.6, 0.6, 1.0, 1),
-	"LIGHT_GREY": (0.9, 0.9, 0.9, 1),
 	"LIGHT_RED": (1.0, 0.6, 0.6, 1),
 	"LIGHT_GREEN": (0.6, 1.0, 0.6, 1),
 }
 
-
 def to_rgb(a_gl_color):
 	return tuple([int(x * 255) for x in a_gl_color])
 
-
 COLOR_NAMES_255 = {k: to_rgb(v) for k, v in COLOR_NAMES.items()}
+#endregion colours
 
 IMAGES = {
 	"background": "images/space.jpg",
 }
 
-class RenderablePlanet:
-	def __init__(self, planet):
-		pass
+class RenderableEntity:
+	def __init__(self, entity):
+		self.entity = entity
 
-class RenderableFleet:
-	def __init__(self, planet):
-		pass
+	def update(self, displayproperty):
+		self.label.text = str(self.entity.__getattribute__(displayproperty))
+
+class RenderablePlanet(RenderableEntity):
+	def __init__(self, planet, batch, displayproperty, colour):
+		super().__init__(planet)
+		self.circles = [
+			pyglet.shapes.Circle(
+				planet.x,
+				planet.y,
+				planet.growth * PLANET_RADIUS_FACTOR,
+				color=COLOR_NAMES_255["WHITE"],
+				batch=batch
+			),
+			pyglet.shapes.Circle(
+				planet.x,
+				planet.y,
+				planet.growth * PLANET_RADIUS_FACTOR-1,
+				color=colour,
+				batch=batch
+			)
+		]
+		self.label = pyglet.text.Label(
+			str(planet.__getattribute__(displayproperty)),
+			x=planet.x,
+			y=planet.y+4,	#centering is weirdly off
+			anchor_x="center", 
+			anchor_y="center",
+			batch=batch
+		)
+
+
+
+class RenderableFleet(RenderableEntity):
+	def __init__(self, fleet, batch, displayproperty, colour):
+		super().__init__(fleet)
+		self.triangle = pyglet.shapes.Triangle(
+				0,										#x
+				0 + fleet.ships * PLANET_RADIUS_FACTOR/2,	#y
+				0 + fleet.ships * PLANET_RADIUS_FACTOR/2,	#x2
+				0 - fleet.ships * PLANET_RADIUS_FACTOR/2,	#y2
+				0 - fleet.ships * PLANET_RADIUS_FACTOR/2,	#x3
+				0 - fleet.ships * PLANET_RADIUS_FACTOR/2,	#y3
+				color=colour,
+				batch=batch
+			)
+		self.triangle.anchor_x = fleet.x
+		self.triangle.anchor_y = fleet.y
+		self.label = pyglet.text.Label(
+			str(fleet.__getattribute__(displayproperty)),
+			x=fleet.x,
+			y=fleet.y+4,	#centering is weirdly off
+			anchor_x="center", 
+			anchor_y="center",
+			batch=batch,
+			font_size=10
+		)
+
+	def update(self, displayproperty):
+		super.update(displayproperty)
+		self.triangle.anchor_x = self.entity.x
+		self.triangle.anchor_y = self.entity.y
 
 class PlanetWarsEntityRenderer:
 	# handles drawing/cached pos/size of PlanetWars game instance for a GUI
@@ -46,92 +107,70 @@ class PlanetWarsEntityRenderer:
 	def __init__(self, game, window):
 		self.game = game
 		#set the playour colours
-		self.playercolours = {}
+		self.playercolours = {NEUTRAL_ID: "GREY"}
 		for p in game.players:
-			#players can't be BLACK or WHITE
-			for k in list(COLOR_NAMES.keys())[2:]:
+			if p == NEUTRAL_ID:
+				continue
+			#players can't be BLACK, WHITE, GREY or LIGHT GREY
+			for k in list(COLOR_NAMES.keys())[4:]:
 				#colours must be unique
+				#TODO: check for running out of colours
 				if k not in self.playercolours.values():
 					self.playercolours[p] = k
+					break
+		self.batch = pyglet.graphics.Batch()
+		self.displayproperty = "ships"
+		self.sync_all()
 
 	def draw(self):
+		#dirty is set whenever a planet changes hands, a fleet is created
+		if self.game.dirty:
+			self.sync_all()
+		# update planet text labels
+		for planet in self.renderableplanets:
+			planet.update(self.displayproperty)
 		# draw planets
+		self.batch.draw()
+
+	def sync_all(self):
+		#sync_all and dirty are used to update the renderer when:
+		#planet ownership changes
+		#fleets are created and destroyed
+		self.renderableplanets = []
 		for p in self.game.planets.values():
-			pyglet.shapes.circle(p.pos, p.radius, color=p.color, filled=True)
-			pyglet.shapes.circle(p.pos, p.radius, color=COLOR_NAMES["WHITE"], filled=False)
-			if p.owner != NEUTRAL_ID:  # 0 == neutral_id
-				pyglet.shapes.circle(p.pos, p.view_radius, color=p.color, filled=False)
-			
-			p.label.draw()
-		# draw fleets
+			#something something view type
+			self.renderableplanets.append(
+				RenderablePlanet(
+					p, 
+					self.batch, 
+					self.displayproperty, 
+					COLOR_NAMES_255[self.playercolours[p.owner]] #bad practice to have properties of p accessed here
+				))
+
+		self.renderablefleets = []
 		for f in self.game.fleets.values():
-			self.pyglet.shapes(f.pos, f.radius, color=f.color, filled=False)
-			f.label.draw()
+			#something something view type
+			self.renderablefleets.append(
+				RenderableFleet(
+					f, 
+					self.batch, 
+					self.displayproperty, 
+					COLOR_NAMES_255[self.playercolours[f.owner]] #bad practice to have properties of p accessed here
+				))
+		self.game.dirty = False
 
-	def sync_all(self, view_id=0, label_type="ships"):
-		# todo: only need to update label values and owner colour details of planets
-		# recache all planets/fleets
-		if view_id == 0:
-			planets = self.game.planets
-			fleets = self.game.fleets
-		else:
-			planets = self.game.players[view_id].planets
-			fleets = self.game.players[view_id].fleets
-
-		# Set which label_type detail to show (id, num_ships, vision etc)
-		for k, p in planets.items():
-			self.planets[k] = self._planet_stamp(p, p.__getattribute__(label_type))
-
-		self.fleets.clear()
-		for k, f in fleets.items():
-			self.fleets[k] = self._fleet_stamp(f, f.num_ships)
-
-	def _planet_stamp(self, planet, value):
-		pos = self.game_to_screen(planet.x, planet.y)
-		radius = (self.ratio * PLANET_MIN_R) + (
-			(PLANET_FACTOR * self.ratio) * planet.growth_rate
-		)
-		view_radius = planet.vision_range() * self.ratio
-		color = COLOR[planet.owner_id]
-		label = Label(
-			str(value),
-			color=COLOR_NAMES_255["BLACK"],
-			x=pos[0],
-			y=pos[1],
-			anchor_x="center",
-			anchor_y="center",
-		)
-		return ScreenPlanet(pos, planet.owner_id, radius, view_radius, color, label)
-
-	def _fleet_stamp(self, fleet, value):
-		pos = self.game_to_screen(fleet.x, fleet.y)
-		radius = 20.0
-		view_radius = fleet.vision_range() * self.ratio
-		color = COLOR[fleet.owner_id]
-		label = Label(
-			str(value),
-			color=COLOR_NAMES_255["WHITE"],
-			x=pos[0],
-			y=pos[1],
-			anchor_x="center",
-			anchor_y="center",
-		)
-		return ScreenFleet(pos, fleet.owner_id, radius, view_radius, color, label)
-
-	def game_to_screen(self, wx, wy):
-		# convert xy values from game space to screen space
-		x = ((wx - self.min_x) / self.dx) * self.display_dx + self.margin
-		y = ((wy - self.min_y) / self.dy) * self.display_dy + self.margin
-		return (x, y)
+	def update():
+		#todo upate text labels after every game loop
+		pass
 
 
 class PlanetWarsUI:
+
 	def __init__(self, window):
-		self.label_type = "num_ships"
+		self.label_type = "ships"
 		self.fps_display = pyglet.window.FPSDisplay(window)
 		self.step_label = pyglet.text.Label(
-			"STEP", x=5, y=window.height - 20, color=COLOR_NAMES_255["WHITE"]
-		)
+			"STEP", x=5, y=window.height - 20, color=COLOR_NAMES_255["WHITE"])
 
 	def draw(self):
 		self.fps_display.draw()
@@ -139,11 +178,25 @@ class PlanetWarsUI:
 
 
 class PlanetWarsWindow(pyglet.window.Window):
+
 	def __init__(self, game):
+		self.game = game
+		#get the window size based on the space occupied by the planets
+		self.extent = {'x': 0, 'y': 0, 'w': 0, 'h': 0}
+		# if planets have negative coords, we don't draw them
+		for planet in self.game.planets.values():
+			if planet.x > self.extent['w']:
+				self.extent['w'] = planet.x
+			if planet.y > self.extent['h']:
+				self.extent['h'] = planet.y
+		#add a margin so no planet is drawn off the edge of the screen
+		self.extent['w'] += 50
+		self.extent['h'] += 50
+
 		#init the Pyglet window
 		super(PlanetWarsWindow, self).__init__(
-			game.extent["w"],
-			game.extent["h"],
+			self.extent["w"],
+			self.extent["h"],
 			vsync=True,
 			resizable=False,
 			caption="Planet Wars",
@@ -153,7 +206,7 @@ class PlanetWarsWindow(pyglet.window.Window):
 		#and it's not resizable, so we want it pretty high on the screen
 		#essentially there's a 1:1 map between screen size and map size that can be displayed
 		self.set_location(10, 10)
-		
+
 		#load the background and scale it to the widnow size
 		self.bg = pyglet.sprite.Sprite(pyglet.image.load(".\\images\\space.jpg"))
 		self.bg.scale_x = self.width / self.bg.image.width
@@ -161,108 +214,74 @@ class PlanetWarsWindow(pyglet.window.Window):
 		#load the global UI elements (FPS counter, etc.)
 		self.ui = PlanetWarsUI(self)
 		#load the renderer for game elements
-		self.gamerenderer = PlanetWarsEntityRenderer(game, self)
+		self.gamerenderer = PlanetWarsEntityRenderer(self.game, self)
 
-		# pyglet.clock.schedule_interval(game.update, 1/60.)
+		pyglet.clock.schedule_interval(game.update, 1/60.)
 		pyglet.app.run()
-
-	def reset_space(self):
-		self.adaptor.screen_resize(self.width, self.height)
-		self.adaptor.sync_all(self.view_id, self.label_type)
 
 	def update(self, args):
 		# gets called by the scheduler at the step_fps interval set
-		game = self.game
-		if game:
+		if self.game:
 			if not self.paused:
-				game.update()
-				self.adaptor.sync_all()
+				self.gamerenderer.update()
+				self.game.update()
 
 			# update step label
-			msg = "Step:" + str(game.tick)
+			msg = "Step:" + str(self.game.tick)
 			if self.paused:
 				msg += " [PAUSED]"
-			msg += ", View:" + str(self.view_id)
-			if self.view_id in game.players:
-				msg += " " + game.players[self.view_id].name
+			msg += f' -- POV: [{self.view_id}] '
+			if self.view_id in self.game.players:
+				msg += f' BOT = {self.game.players[self.view_id].name}'
 			elif self.view_id == 0:
-				msg += " All "
-			msg += str(game.tick)
+				msg += ' All '
+			msg += str(self.game.tick)
 			msg += " Show: " + self.label_type
 
 			self.step_label.text = msg
 			# Has the game ended? (Should we close?)
 			if not self.game.is_alive() or self.game.tick >= self.max_tick:
 				self.close()
+			if self.game.dirty:
+				self.gamerenderer.sync_all()
 		else:
 			self.step_label.text = "---"
 
-	def set_pen_color(self, color=None, name=None):
-		if name is not None:
-			color = COLOR_NAMES[name]
-		self.curr_color = color
-		glColor4f(*self.curr_color)
-
-	def set_stroke(self, stroke):
-		self.stroke = stroke
-		glLineWidth(self.stroke)
-
-	def circle(self, pos, radius, color=None, filled=True):
-		if color:
-			self.set_pen_color(color)
-		if filled:
-			gluQuadricDrawStyle(self.qobj, GLU_FILL)
-		else:
-			gluQuadricDrawStyle(self.qobj, GLU_SILHOUETTE)
-		glPushMatrix()
-		glTranslatef(pos[0], pos[1], 0.0)
-		gluDisk(self.qobj, 0, radius, 32, 1)
-		glPopMatrix()
-
-	def line(self, x1=0, y1=0, x2=0, y2=0, pos1=None, pos2=None):
-		"""Draw a single line. Either with xy values, or two position (that
-		contain x and y values). Uses existing colour and stroke values."""
-		if pos1 is not None and pos2 is not None:
-			x1, y1, x2, y2 = pos1[0], pos1[1], pos2[0], pos2[1]
-		glBegin(GL_LINES)
-		glVertex2f(x1, y1)
-		glVertex2f(x2, y2)
-		glEnd()
 
 	def on_key_press(self, symbol, modifiers):
 		# Single Player View, or All View
-		if symbol == key.BRACKETLEFT:
+		if symbol == pyglet.window.key.BRACKETLEFT:
 			self.view_id = (
-				self.view_id - 1 if self.view_id > 1 else len(self.game.players)
-			)
-		if symbol == key.BRACKETRIGHT:
+				self.view_id - 1 if self.view_id > 1 else len(self.game.players))
+		if symbol == pyglet.window.key.BRACKETRIGHT:
 			self.view_id = (
-				self.view_id + 1 if self.view_id < len(self.game.players) else 1
-			)
+				self.view_id + 1 if self.view_id < len(self.game.players) else 1)
 		# Everyone view
-		elif symbol == key.A:
+		elif symbol == pyglet.window.key.A:
 			self.view_id = 0  # == "all"
 		# Planet attribute type to show?
-		elif symbol == key.L:
+		elif symbol == pyglet.window.key.L:
 			i = self.label_type
-			l = ["id", "num_ships", "vision_age", "owner_id"]
+			l = ["id", "ships", "vision_age", "owner_id"]
 			self.label_type = l[l.index(i) + 1] if l.index(i) < (len(l) - 1) else l[0]
 		# Reset?
-		elif symbol == key.R:
+		elif symbol == pyglet.window.key.R:
 			self.reset_space()
 		# Do one step
-		elif symbol == key.N:
+		elif symbol == pyglet.window.key.N:
 			self.game.update()
 		# Pause toggle?
-		elif symbol == key.P:
+		elif symbol == pyglet.window.key.P:
 			self.paused = not self.paused
 		# Speed up (+) or slow down (-) the sim
-		elif symbol in [key.PLUS, key.EQUAL]:
-			self.set_fps(self.fps + 5)
-		elif symbol == key.MINUS:
-			self.set_fps(self.fps - 5)
+		elif symbol in [pyglet.window.key.PLUS, pyglet.window.key.EQUAL]:
+			self.set_fps(pyglet.window.fps + 5)
+		elif symbol == pyglet.window.key.MINUS:
+			self.set_fps(pyglet.window.fps - 5)
+		elif symbol == pyglet.window.key.ESCAPE:
+			pyglet.app.exit()
 
-		self.adaptor.sync_all(self.view_id, self.label_type)
+		#self.adaptor.sync_all(self.view_id, self.label_type)
 
 	def on_draw(self):
 		self.clear()
