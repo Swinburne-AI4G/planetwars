a3551a83200a79163a4f601286873eef03a01967
James Bonner <jabcmes@outlook.com>
2023-04-15 21:06:19 +1000

    debug bot

--- bots/OneSlowMove.py
+++ bots/OneSlowMove.py
@@ -1,13 +1,10 @@
 class OneSlowMove(object):
-    def __init__(self) -> None:
-        self.counter = 0
+	def __init__(self) -> None:
+		self.counter = 0
 
-    def update(self, gameinfo):
-        if self.counter > 100:
-            self.counter = 0
-            src = list(gameinfo._my_planets().values())[0]
-            dest = list(gameinfo._not_my_planets().values())[0]
-            gameinfo.planet_order(src, dest, src.ships)
-            #gameinfo.log("I'll send %d ships from planet %s to planet %s" % (src.ships, src, dest))
-        else: 
-            self.counter += 1
+	def update(self, gameinfo):
+		src = list(gameinfo._my_planets().values())[0]
+		if src.ships > 100:
+			dest = list(gameinfo._not_my_planets().values())[0]
+			gameinfo.planet_order(src, dest, src.ships)
+			#gameinfo.log("I'll send %d ships from planet %s to planet %s" % (src.ships, src, dest))
