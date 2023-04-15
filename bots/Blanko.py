99dd771b4365bae129d781e4a41552c1abb09300
James Bonner <jabcmes@outlook.com>
2023-04-15 21:00:03 +1000

    changed whitespace

--- bots/Blanko.py
+++ bots/Blanko.py
@@ -15,36 +15,36 @@ you want to know more you'll need to scout!
 
 A gameinfo instance has various (possibly useful) dict's for you to use:
 
-    # all planet and fleets (that you own or can see)
-    planets
-    fleets
+	# all planet and fleets (that you own or can see)
+	planets
+	fleets
 
-    # dict's of just your planets/fleets
-    my_planets
-    my_fleets
+	# dict's of just your planets/fleets
+	my_planets
+	my_fleets
 
-    # dict's of both neutral and enemy planets/fleets 
-    not_my_planets
-    not_my_fleets
+	# dict's of both neutral and enemy planets/fleets 
+	not_my_planets
+	not_my_fleets
 
-    # dict's of just the enemy planet/fleets (fog limited)
-    enemy_planets
-    enemy_fleets
+	# dict's of just the enemy planet/fleets (fog limited)
+	enemy_planets
+	enemy_fleets
 
 You issue orders from your bot using the methods of the gameinfo instance. 
 
-    gameinfo.planet_order(src, dest, ships)
-    gameinfo.fleet_order(src, dest, ships) 
+	gameinfo.planet_order(src, dest, ships)
+	gameinfo.fleet_order(src, dest, ships) 
 
 For example, to send 10 ships from planet src to planet dest, you would
 say `gameinfo.planet_order(src, dest, 10)`.
 
 There is also a player specific log if you want to leave a message
 
-    gameinfo.log("Here's a message from the bot")
+	gameinfo.log("Here's a message from the bot")
 
 '''
 
 class Blanko(object):
-    def update(self, gameinfo):
-        pass
+	def update(self, gameinfo):
+		pass
