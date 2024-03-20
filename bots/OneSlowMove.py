f4c1d4bb1f133048f47f63b994a9d92d2c64017c
James Bonner <jabcmes@outlook.com>
2024-03-21 00:55:30 +1100

    more dynamic test AI

--- bots/OneSlowMove.py
+++ bots/OneSlowMove.py
@@ -4,7 +4,8 @@ class OneSlowMove(object):
 
 	def update(self, gameinfo):
 		src = list(gameinfo._my_planets().values())[0]
-		if src.ships > 100:
+		self.counter += 0.5
+		if src.ships > self.counter:
 			dest = list(gameinfo._not_my_planets().values())[0]
 			gameinfo.planet_order(src, dest, src.ships)
 			#gameinfo.log("I'll send %d ships from planet %s to planet %s" % (src.ships, src, dest))
